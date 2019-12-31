295a5833f89bbad909669acc3e342cbe59aa95d1
liunian <a@liunian.io>
2019-12-31 17:35:40 +0000

    计算逾期费

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // 每分钟执行一次
         $schedule->command('cron:finish-crowdfunding')->everyMinute();
+        $schedule->command('cron:calculate-installment-fine')->daily();
     }
 
     /**
