7e89e5fc94b574005dcb9b1bc37cc36478d7a9c2
liunian <a@liunian.io>
2019-12-31 18:20:34 +0000

    商品属性模型

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Exceptions\CouponCodeUnavailableException;
 use App\Exceptions\InternalException;
+use App\Jobs\RefundInstallmentOrder;
 use App\Models\CouponCode;
 use App\Models\User;
 use App\Models\UserAddress;
@@ -134,6 +135,7 @@ class OrderService
 
         return $order;
     }
+
     public function refundOrder(Order $order)
     {
         // 判断该订单的支付方式
@@ -142,41 +144,49 @@ class OrderService
                 // 生成退款订单号
                 $refundNo = Order::getAvailableRefundNo();
                 app('wechat_pay')->refund([
-                    'out_trade_no' => $order->no,
-                    'total_fee' => $order->total_amount * 100,
-                    'refund_fee' => $order->total_amount * 100,
+                    'out_trade_no'  => $order->no,
+                    'total_fee'     => $order->total_amount * 100,
+                    'refund_fee'    => $order->total_amount * 100,
                     'out_refund_no' => $refundNo,
-                    'notify_url' => ngrok_url('payment.wechat.refund_notify'),
+                    'notify_url'    => ngrok_url('payment.wechat.refund_notify'),
                 ]);
                 $order->update([
-                    'refund_no' => $refundNo,
+                    'refund_no'     => $refundNo,
                     'refund_status' => Order::REFUND_STATUS_PROCESSING,
                 ]);
                 break;
             case 'alipay':
                 $refundNo = Order::getAvailableRefundNo();
                 $ret = app('alipay')->refund([
-                    'out_trade_no' => $order->no,
-                    'refund_amount' => $order->total_amount,
+                    'out_trade_no'   => $order->no,
+                    'refund_amount'  => $order->total_amount,
                     'out_request_no' => $refundNo,
                 ]);
                 if ($ret->sub_code) {
                     $extra = $order->extra;
                     $extra['refund_failed_code'] = $ret->sub_code;
                     $order->update([
-                        'refund_no' => $refundNo,
+                        'refund_no'     => $refundNo,
                         'refund_status' => Order::REFUND_STATUS_FAILED,
-                        'extra' => $extra,
+                        'extra'         => $extra,
                     ]);
                 } else {
                     $order->update([
-                        'refund_no' => $refundNo,
+                        'refund_no'     => $refundNo,
                         'refund_status' => Order::REFUND_STATUS_SUCCESS,
                     ]);
                 }
                 break;
+            case 'installment':
+                $order->update([
+                    'refund_no'     => Order::getAvailableRefundNo(), // 生成退款订单号
+                    'refund_status' => Order::REFUND_STATUS_PROCESSING, // 将退款状态改为退款中
+                ]);
+                // 触发退款异步任务
+                dispatch(new RefundInstallmentOrder($order));
+                break;
             default:
-                throw new InternalException('未知订单支付方式：'.$order->payment_method);
+                throw new InternalException('未知订单支付方式：' . $order->payment_method);
                 break;
         }
     }
